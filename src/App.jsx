de801d64dc17716930de4d0a24a5ab689e48d773
Eric Casanova <kilbane1407@gmail.com>
2024-02-06 19:03:22 -0300

    creada pag grow your business

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage/MainPage";
 import Result from "./pages/Result/Result";
-import { GENERATE_PLAN, MAIN } from "./constants/routes";
+import GrowYourBusiness from "./pages/GrowYourBusiness/GrowYourBusiness";
+import { GENERATE_PLAN, MAIN, GROW_YOUR_BUSINESS } from "./constants/routes";
 function App() {
   return (
     <Routes>
       <Route path={MAIN} element={<MainPage />} />
       <Route path={GENERATE_PLAN + "/:id"} element={<Result />} />
+      <Route path={GROW_YOUR_BUSINESS} element={<GrowYourBusiness />} />
     </Routes>
   );
 }
--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,2 +1,3 @@
 export const MAIN = "/";
 export const GENERATE_PLAN = "/result";
+export const GROW_YOUR_BUSINESS = "/grow";
